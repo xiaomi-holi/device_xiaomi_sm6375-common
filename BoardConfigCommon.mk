42478e9cc9b44c5ee963e31c7096703472dfa3e4
Agatha sen <naufalmarjan2014@gmail.com>
2023-10-06 08:05:02 +0000

    sm6375-common: Update blobs to moonstone MIUI V14.0.5.0.TMPMIXM relea… …se-keys

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,7 +188,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-07-01
+VENDOR_SECURITY_PATCH := 2023-08-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
